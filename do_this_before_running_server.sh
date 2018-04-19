7088c31a39662539ee8807a841dfffade5013e23
Adam <avvallky@gmail.com>
2018-04-19 10:37:34 -0400

    Update do_this_before_running_server.sh

--- do_this_before_running_server.sh
+++ do_this_before_running_server.sh
@@ -1,6 +1,17 @@
 cd ./models
+# Tensorflow and dependency installation
+# pick one of the two tensorflow install options depending on if you have a gpu or not
+pip install tensorflow
+#pip install tensorflow-gpu
+sudo apt-get install protobuf-compiler python-pil python-lxml python-tk
+sudo pip install Cython
+sudo pip install jupyter
+sudo pip install matplotlib
 protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+# a test to see if the tensorflow installation went correctly
+#python object_detection/builders/model_builder_test.py 
+
+# Now to run the server
 cd ..
-#python object_detection/builders/model_builder_test.py
 python deploy_server.py
